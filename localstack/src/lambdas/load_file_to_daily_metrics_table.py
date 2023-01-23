57602d3b43e7bb7ad9ace422333d51b4e10257dc
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2023-01-23 19:05:24 +0300

    Added load_file_to_daily_metrics_table.py

---
+++ localstack/src/lambdas/load_file_to_daily_metrics_table.py
@@ -0,0 +1,40 @@
+import json
+import os
+from decimal import Decimal
+
+import boto3
+import pandas as pd
+
+
+def lambda_handler(event, context):
+    aws_endpoint_url = os.getenv('AWS_ENDPOINT_URL')
+    aws_access_key = os.getenv('AWS_ACCESS_KEY')
+    aws_secret_key = os.getenv('AWS_SECRET_KEY')
+    aws_region_name = os.getenv('AWS_REGION_NAME')
+    s3_resource = boto3.resource('s3',
+                                 endpoint_url=aws_endpoint_url,
+                                 aws_access_key_id=aws_access_key,
+                                 aws_secret_access_key=aws_secret_key,
+                                 region_name=aws_region_name)
+    dynamodb_resource = boto3.resource('dynamodb',
+                                       endpoint_url=aws_endpoint_url,
+                                       aws_access_key_id=aws_access_key,
+                                       aws_secret_access_key=aws_secret_key,
+                                       region_name=aws_region_name)
+    helsinki_city_bikes_daily_metrics_table = dynamodb_resource.Table('helsinki_city_bikes_daily_metrics')
+    event_body = event['Records'][0]['Sns']['Message']
+    helsinki_city_bikes_key = json.loads(event_body)['Records'][0]['s3']['object']['key']
+    helsinki_city_bikes_df = pd.read_csv(s3_resource.Object(bucket_name='helsinki-city-bikes',
+                                                            key=helsinki_city_bikes_key)
+                                         .get()['Body'],
+                                         parse_dates=['departure']) \
+        .fillna(0)
+    for group_name, group in helsinki_city_bikes_df.groupby(pd.Grouper(key='departure', freq='D')):
+        if not group.empty:
+            with helsinki_city_bikes_daily_metrics_table.batch_writer(overwrite_by_pkeys=['date']) as batch_writer:
+                batch_writer.put_item(
+                    Item={'date': f'{group_name:%Y-%m-%d}',
+                          'avg_distance_m': Decimal(str(helsinki_city_bikes_df['distance (m)'].mean())),
+                          'avg_duration_sec': Decimal(str(helsinki_city_bikes_df['duration (sec.)'].mean())),
+                          'avg_speed_km_h': Decimal(str(helsinki_city_bikes_df['avg_speed (km/h)'].mean())),
+                          'avg_air_temperature_c': Decimal(str(helsinki_city_bikes_df['Air temperature (degC)'].mean()))})
